4a625b8857bd825ea75ad18772be97a8bf2184f0
Stanislav Semenov <stanislav.semenov@tabby.ai>
2024-02-15 11:33:55 +0300

    firts

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import styles from './Button.module.css';
+
+const Button = ({ text, onClick }) => {
+    return (
+        <button className={styles.button} onClick={onClick}>
+            {text}
+        </button>
+    );
+};
+
+export default Button;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [['@babel/plugin-transform-react-jsx', { pragma: 'h', pragmaFrag: 'Fragment' }]]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            Components: path.resolve(__dirname, 'src/components/'),
+        },
+        extensions: ['.js', '.jsx']
+    }
+};
